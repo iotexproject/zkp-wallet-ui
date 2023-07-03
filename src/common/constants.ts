ce88125582ac855331d6590e1bf8c047a9b7476e
ququzone <xueping.yang@gmail.com>
2023-07-04 07:11:41 +0800

    add contracts

---
+++ src/common/constants.ts
@@ -0,0 +1,8 @@
+export const addresses = {
+    "4690": {
+        entryPoint: "0x16653fF93F0e5fEF85CE46862562a4c58B05435E",
+        accountFactory: "0x75e61FEDd70f6015282e28d3De87bDd7c6F66dD0",
+        registry: "0x845d8ccb0D92174B33AC9A876B65c49Ca4676685",
+        nft: "0xA3Ce183b2EA38053f85A160857E6f6A8C10EF5f7",
+    }
+}
