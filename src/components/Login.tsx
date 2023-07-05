0d902544e92c1305c70121d5004ea3b2909b8a9b
ququzone <xueping.yang@gmail.com>
2023-07-06 00:44:02 +0800

    complete mint workflow

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -30,7 +30,7 @@ export const Login = observer(() => {
                     </div>
                     <div className="mt-2">
                         <input onChange={e => {
-                            base.username = e.target.value
+                            base.password = e.target.value
                         }} id="password" name="password" type="password" autoComplete="current-password"
                             required
                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 pl-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -48,7 +48,7 @@ export const Login = observer(() => {
                             <button
                                 type="button"
                                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                onClick={() => base.claim()}
+                                onClick={() => base.login()}
                             >Create wallet & Mint
                             </button>
                         )
--- src/store/base.ts
+++ src/store/base.ts
@@ -1,11 +1,15 @@
 import { makeAutoObservable } from 'mobx'
-import {providers} from "ethers"
-import {ZKPassAccount__factory} from "../contracts/ZKPassAccount__factory"
-import {ZKPassAccountFactory__factory} from "../contracts/ZKPassAccountFactory__factory"
-import {INSRegistry__factory} from "../contracts/INSRegistry__factory"
-import {EntryPoint__factory} from "@account-abstraction/contracts"
+import { constants, providers } from "ethers"
+import { ZKPassAccount__factory } from "../contracts/ZKPassAccount__factory"
+import { ZKPassAccountFactory__factory } from "../contracts/ZKPassAccountFactory__factory"
+import { INSRegistry__factory } from "../contracts/INSRegistry__factory"
+import { EntryPoint__factory } from "@account-abstraction/contracts"
 import { addresses } from "../common/constants"
 import { ZKPWalletNFT__factory } from '../contracts/ZKPWalletNFT__factory'
+import { hexConcat, hexlify, keccak256, namehash, resolveProperties, toUtf8Bytes } from 'ethers/lib/utils'
+import { ZKPSigner, prove } from '../signer'
+import { PublicResolver__factory } from '../contracts/PublicResolver__factory'
+import { deepHexlify, fillUserOp, signOp } from '../signer/utils'
 
 const config = addresses["testnet"]
 
@@ -31,17 +35,129 @@ export class BaseStore {
     nft = ZKPWalletNFT__factory.connect(config.nft, this.provider)
     disableButton = false
 
+    account = {
+        created: false,
+        username: "",
+        password: "",
+        address: "",
+    }
+
     constructor() {
         makeAutoObservable(this)
     }
 
-    claim() {
+    async login() {
+        if (this.username.trim() === "") {
+            this.info = {
+                show: true,
+                text: 'Account name is empty'
+            }
+            return
+        }
+        if (this.password.trim() === "") {
+            this.info = {
+                show: true,
+                text: 'Password is empty'
+            }
+            return
+        }
         this.disableButton = true
+        this.account.username = this.username.trim()
+        this.account.password = this.password.trim()
+
+        const nameHash = namehash(this.account.username + ".zkwallet.io")
+        const resovler = await this.registry.resolver(nameHash)
+
         this.info = {
             show: true,
-            text: 'Hello'
+            text: 'Generate password proof...'
         }
-        console.log(this.username)
+        const passport = BigInt(keccak256(
+            hexConcat([nameHash, hexlify(toUtf8Bytes(this.account.password))])
+        ))
+        const {publicSignals} = await prove(
+            BigInt(0), // nonce
+            BigInt(0), // opHash
+            passport,
+            "passport.wasm",
+            "passport_0001.zkey"
+        )
+        if (resovler === constants.AddressZero) {
+            this.account.address = await this.factory.getAddress(this.account.username, publicSignals[0])
+        } else {
+            const publicRessolver = PublicResolver__factory.connect(resovler, this.provider)
+            this.account.address = await publicRessolver["addr(bytes32)"](nameHash)
+            this.account.created = true
+
+            const account = ZKPassAccount__factory.connect(this.account.address, this.provider)
+            const passHash = await account.passHash()
+            if (passHash.toString() !== publicSignals[0].toString()) {
+                this.disableButton = false
+                this.info = {
+                    show: true,
+                    text: 'Password incorrect'
+                }
+                return
+            }
+        }
+        const op = {
+            sender: this.account.address,
+            callData: this.accountTpl.interface.encodeFunctionData("execute", [
+                config.nft,
+                0,
+                "0x1249c58b",
+            ]),
+            callGasLimit: 70000,
+            preVerificationGas: 60000
+        }
+        if (!this.account.created) {
+            // @ts-ignore
+            op.initCode = hexConcat([
+                this.factory.address,
+                this.factory.interface.encodeFunctionData("createAccount", [this.account.username, publicSignals[0]]),
+            ])
+        }
+        const fullOp = await fillUserOp(op, this.entryPoint)
+        let hexifiedUserOp = deepHexlify(await resolveProperties(fullOp))
+        this.info = {
+            show: true,
+            text: 'Request paymaster signature...'
+        }
+        let result = await this.paymaster.send("eth_signVerifyingPaymaster", [hexifiedUserOp])
+        fullOp.paymasterAndData = result
+
+        const chainId = (await this.provider.getNetwork()).chainId
+        this.info = {
+            show: true,
+            text: 'Sign user operation using zk prover...'
+        }
+        const signedOp = await signOp(
+            fullOp,
+            this.entryPoint.address,
+            chainId,
+            new ZKPSigner(nameHash, this.account.password, fullOp.nonce)
+        )
+        hexifiedUserOp = deepHexlify(await resolveProperties(signedOp))
+
+        this.info = {
+            show: true,
+            text: 'Send user operation to bundler...'
+        }
+        result = await this.bundler.send("eth_sendUserOperation", [hexifiedUserOp, this.entryPoint.address])
+
+        if (this.account.created) {
+            this.info = {
+                show: true,
+                text: `Mint NFT opHash: ${result}`
+            }
+        } else {
+            this.info = {
+                show: true,
+                text: `Create account and mint NFT opHash: ${result}`
+            }
+        }
+        this.isLogin = true
+        this.disableButton = false
     }
 
     async fetchMintedNFT() {
