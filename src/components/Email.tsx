9a664ccc97d4c5d03d95781f73e7cd06975bc76e
ququzone <xueping.yang@gmail.com>
2024-02-14 10:40:08 +0800

    request verify code

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -31,7 +31,7 @@ export const Email = observer(() => {
                         type="button"
                         className="flex mt-10 w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         onClick={() => base.sendCode()}
-                    >Send verify code</button>
+                    >Requst verify code</button>
                     {base.disableButton?
                     <button
                         type="button" disabled
